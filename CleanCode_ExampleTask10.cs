baee38c89135f5f9c0ffdcbeaec11a98978c71c3
Crackbite <94061798+Crackbite@users.noreply.github.com>
2022-06-18 20:02:56 +0500

    Task solved #1

--- CleanCode_ExampleTask10.cs
+++ CleanCode_ExampleTask10.cs
@@ -1,8 +1,10 @@
 class Weapon
 {
+    private const int BulletsPerShot = 1;
+
     private int _bullets;
 
-    public bool CanShoot() => _bullets > 0;
+    public bool CanShoot() => _bullets >= BulletsPerShot;
 
-    public void Shoot() => _bullets -= 1;
+    public void Shoot() => _bullets -= BulletsPerShot;
 }
