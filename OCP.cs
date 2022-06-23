5181be713950516b8dc155ceb4f4fda505d8388b
Crackbite <94061798+Crackbite@users.noreply.github.com>
2022-06-24 00:31:40 +0500

    Task solved #1

--- OCP.cs
+++ OCP.cs
@@ -1,53 +1,132 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace IMJunior
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            var orderForm = new OrderForm();
-            var paymentHandler = new PaymentHandler();
+            var paymentSystems = new List<PaymentSystem>
+            {
+                new Qiwi(),
+                new WebMoney(),
+                new Card()
+            };
 
-            var systemId = orderForm.ShowForm();
+            var orderForm = new OrderForm(paymentSystems);
 
-            if (systemId == "QIWI")
-                Console.WriteLine("Перевод на страницу QIWI...");
-            else if (systemId == "WebMoney")
-                Console.WriteLine("Вызов API WebMoney...");
-            else if (systemId == "Card")
-                Console.WriteLine("Вызов API банка эмитера карты Card...");
+            orderForm.Show();
+            bool paymentSystemSelected = orderForm.TrySelectPaymentSystem(out PaymentSystem paymentSystem);
 
-            paymentHandler.ShowPaymentResult(systemId);
+            if (paymentSystemSelected == false)
+            {
+                Console.WriteLine("Выбранной системы не существует!");
+                Console.ReadKey();
+                return;
+            }
+
+            paymentSystem.MakePayment();
+            paymentSystem.ShowPaymentResult();
+
+            Console.ReadKey();
         }
     }
 
-    public class OrderForm
+    internal abstract class PaymentSystem
     {
-        public string ShowForm()
+        public abstract string Id { get; }
+
+        public abstract void MakePayment();
+
+        public void ShowPaymentResult()
         {
-            Console.WriteLine("Мы принимаем: QIWI, WebMoney, Card");
-
-            //симуляция веб интерфейса
-            Console.WriteLine("Какое системой вы хотите совершить оплату?");
-            return Console.ReadLine();
-        }
-    }
-
-    public class PaymentHandler
-    {
-        public void ShowPaymentResult(string systemId)
-        {
-            Console.WriteLine($"Вы оплатили с помощью {systemId}");
-
-            if (systemId == "QIWI")
-                Console.WriteLine("Проверка платежа через QIWI...");
-            else if (systemId == "WebMoney")
-                Console.WriteLine("Проверка платежа через WebMoney...");
-            else if (systemId == "Card")
-                Console.WriteLine("Проверка платежа через Card...");
-
+            Console.WriteLine($"Вы оплатили с помощью {Id}");
+            Console.WriteLine($"Проверка платежа через {Id}...");
             Console.WriteLine("Оплата прошла успешно!");
+        }
+
+        public override string ToString()
+        {
+            return Id;
+        }
+    }
+
+    internal sealed class Qiwi : PaymentSystem
+    {
+        public override string Id => "QIWI";
+
+        public override void MakePayment()
+        {
+            Console.WriteLine($"Перевод на страницу {Id}...");
+        }
+    }
+
+    internal sealed class WebMoney : PaymentSystem
+    {
+        public override string Id => "WebMoney";
+
+        public override void MakePayment()
+        {
+            Console.WriteLine($"Вызов API {Id}...");
+        }
+    }
+
+    internal sealed class Card : PaymentSystem
+    {
+        public override string Id => "Card";
+
+        public override void MakePayment()
+        {
+            Console.WriteLine($"Вызов API банка эмитера карты {Id}...");
+        }
+    }
+
+    internal sealed class OrderForm
+    {
+        private readonly List<PaymentSystem> _paymentSystems;
+
+        public OrderForm(List<PaymentSystem> paymentSystems)
+        {
+            if (paymentSystems == null)
+            {
+                throw new ArgumentNullException(nameof(paymentSystems));
+            }
+
+            if (paymentSystems.Count < 1)
+            {
+                throw new ArgumentException(nameof(paymentSystems));
+            }
+
+            _paymentSystems = paymentSystems;
+        }
+
+        public void Show()
+        {
+            Console.Write("Мы принимаем: ");
+
+            for (var i = 0; i < _paymentSystems.Count; i++)
+            {
+                Console.Write(_paymentSystems[i]);
+
+                if (i != _paymentSystems.Count - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+
+            Console.WriteLine(string.Empty);
+        }
+
+        public bool TrySelectPaymentSystem(out PaymentSystem paymentSystem)
+        {
+            // Симуляция веб интерфейса
+            Console.Write("Какое системой вы хотите совершить оплату: ");
+            string systemId = Console.ReadLine();
+
+            paymentSystem = _paymentSystems.FirstOrDefault(system => system.Id == systemId);
+            return paymentSystem != null;
         }
     }
 }
